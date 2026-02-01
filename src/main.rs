f30e68cba47bd179980241a43070b503c749ebf2
Tom Webber <t.c.w.webber@gmail.com>
2026-02-01 12:41:35 +0100

    feat(imap): parse email headers and body

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,59 @@
-use std::env;
-use std::net::TcpStream;
-
+use chrono::DateTime;
+use chrono::FixedOffset;
+use core::iter;
 use encoded_words::decode;
 use imap::types::Flag;
+use mailparse::MailHeaderMap as _;
+use mailparse::ParsedMail;
+use mailparse::parse_mail;
 use native_tls::{TlsConnector, TlsStream};
+use std::env;
+use std::net::TcpStream;
 use utf7_imap::decode_utf7_imap;
+
+fn split_and_parse_header(email: &ParsedMail, name: &str) -> Vec<String> {
+    email
+        .headers
+        .get_first_value(name)
+        .map(|from| from.split(',').map(|s| s.trim().to_string()).collect())
+        .unwrap_or_default()
+}
 
 struct Email(imap::Session<TlsStream<TcpStream>>);
 
 impl Email {
+    fn parse_email(&mut self, uid: u32) -> EmailData {
+        let messages = self.0.fetch(uid.to_string(), "RFC822").unwrap();
+        let message = messages.iter().next().unwrap();
+        let raw = message.body().unwrap();
+        let parsed = parse_mail(raw).unwrap();
+
+        let from = split_and_parse_header(&parsed, "From");
+        let to = split_and_parse_header(&parsed, "To");
+        let cc = split_and_parse_header(&parsed, "Cc");
+        let bcc = split_and_parse_header(&parsed, "Bcc");
+        let subject =
+            parsed.headers.get_first_value("Subject").unwrap_or_default();
+
+        let datetime = DateTime::parse_from_rfc2822(
+            &parsed.headers.get_first_value("Date").unwrap(),
+        )
+        .unwrap();
+
+        let mut txt = String::new();
+        let mut html = String::new();
+
+        for part in parsed.subparts.iter().chain(iter::once(&parsed)) {
+            match part.ctype.mimetype.as_str() {
+                "text/plain" => txt = part.get_body().unwrap_or_default(),
+                "text/html" => html = part.get_body().unwrap_or_default(),
+                _ => {}
+            }
+        }
+
+        EmailData { txt, html, from, to, subject, cc, bcc, datetime }
+    }
+
     fn new() -> Self {
         let domain = env::var("DOMAIN").unwrap();
         let username = env::var("USERNAME").unwrap();
@@ -24,8 +69,6 @@ impl Email {
     }
 
     fn first_inbox_message(&mut self) -> (bool, String) {
-        self.0.select("INBOX").unwrap();
-
         let messages = self.0.fetch("1", "(FLAGS BODY.PEEK[])").unwrap();
         let message = messages.iter().next().unwrap();
 
@@ -44,7 +87,6 @@ impl Email {
     }
 
     fn most_recent(&mut self) -> Vec<String> {
-        self.0.select("INBOX").unwrap();
         let uids = self.0.uid_search("ALL").unwrap();
         let mut uids: Vec<u32> = uids.into_iter().collect();
         uids.sort_unstable();
@@ -68,6 +110,26 @@ impl Email {
             })
             .collect()
     }
+
+    fn find_with_subject(&mut self, subject: &str) -> Option<u32> {
+        let uids = self.0.search("ALL").unwrap();
+
+        for uid in uids {
+            let msgs = self.0.fetch(uid.to_string(), "RFC822").unwrap();
+            let msg = msgs.iter().next().unwrap();
+            let raw = msg.body().unwrap();
+
+            let parsed = mailparse::parse_mail(raw).unwrap();
+            if let Some(subj) = parsed.headers.get_first_value("Subject") {
+                let decoded = decode(&subj).map(|s| s.decoded).unwrap_or(subj);
+                if decoded == subject {
+                    return Some(uid);
+                }
+            }
+        }
+
+        None
+    }
 }
 
 impl Drop for Email {
@@ -76,9 +138,25 @@ impl Drop for Email {
     }
 }
 
+#[derive(Debug)]
+struct EmailData {
+    txt: String,
+    html: String,
+    from: Vec<String>,
+    to: Vec<String>,
+    subject: String,
+    cc: Vec<String>,
+    bcc: Vec<String>,
+    datetime: DateTime<FixedOffset>,
+}
+
 fn main() {
     let mut app = Email::new();
 
+    app.0.select("Sent").unwrap();
+    let uid = app.find_with_subject("Example subject").unwrap();
+    dbg!(app.parse_email(uid));
+    app.0.select("INBOX").unwrap();
     dbg!(app.first_inbox_message());
     dbg!(app.list_boxes());
     dbg!(app.most_recent());

8f6bbb09a0a21586f9464b519b4e642cd54715ac
Tom Webber <t.c.w.webber@gmail.com>
2026-02-01 10:30:13 +0100

    feat(imap): get subject of most recent emails

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::env;
 use std::net::TcpStream;
 
+use encoded_words::decode;
 use imap::types::Flag;
 use native_tls::{TlsConnector, TlsStream};
 use utf7_imap::decode_utf7_imap;
-
-const BODY: &str = "RFC822";
 
 struct Email(imap::Session<TlsStream<TcpStream>>);
 
@@ -43,6 +42,32 @@ impl Email {
             .map(|b| decode_utf7_imap(b.name().to_owned()))
             .collect()
     }
+
+    fn most_recent(&mut self) -> Vec<String> {
+        self.0.select("INBOX").unwrap();
+        let uids = self.0.uid_search("ALL").unwrap();
+        let mut uids: Vec<u32> = uids.into_iter().collect();
+        uids.sort_unstable();
+        let recent = uids
+            .iter()
+            .rev()
+            .take(100)
+            .map(u32::to_string)
+            .collect::<Vec<_>>()
+            .join(",");
+        self.0
+            .uid_fetch(recent.as_str(), "ENVELOPE")
+            .unwrap()
+            .into_iter()
+            .map(|m| {
+                let envelope = m.envelope().unwrap();
+                let string =
+                    String::from_utf8_lossy(envelope.subject.unwrap_or(&[]))
+                        .to_string();
+                decode(&string).map(|x| x.decoded).unwrap_or(string)
+            })
+            .collect()
+    }
 }
 
 impl Drop for Email {
@@ -56,4 +81,5 @@ fn main() {
 
     dbg!(app.first_inbox_message());
     dbg!(app.list_boxes());
+    dbg!(app.most_recent());
 }
